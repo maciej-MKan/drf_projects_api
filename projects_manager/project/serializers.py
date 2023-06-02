22071296024ec002af63b4d8501c6524b971e16d
MKan <kanabaj.maciej@gmail.com>
2023-06-02 19:23:09 +0200

    Update user serializer to restrict get data

--- projects_manager/project/serializers.py
+++ projects_manager/project/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from .models import ProjectUser
 from ..project.models import Project
 
 
--- projects_manager/user/serializers.py
+++ projects_manager/user/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 from .models import ProjectUser
 from ..project.models import Project
@@ -8,4 +9,27 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = ProjectUser
-        fields = ['first_name', 'last_name', 'password', 'age', 'gender', 'email', 'phone_number', 'projects']
+        fields = ['id', 'first_name', 'last_name', 'age', 'gender', 'email', 'password', 'phone_number', 'projects']
+
+    def to_representation(self, instance):
+        request = self.context['request']
+        user = request.user
+        # if request.method == "GET":
+        instance.password = ""
+
+        if not user.is_superuser and user != instance:
+            if instance.is_superuser:
+                return {}
+            return {
+                'id': instance.id,
+                'first_name': instance.first_name,
+                'last_name': instance.last_name
+            }
+        return super().to_representation(instance)
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            validated_data['password'] = make_password(validated_data['password'])
+
+        return super().update(instance, validated_data)
+
