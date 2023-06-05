838191f46b7d76b88f6c626fdc619f3c20a021bf
MKan <kanabaj.maciej@gmail.com>
2023-06-05 09:05:14 +0200

    Add comment validators and permissins

--- projects_manager/comment/serializers.py
+++ projects_manager/comment/serializers.py
@@ -1,9 +1,25 @@
+from datetime import datetime
+import re
+
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from projects_manager.comment.models import Comment
 from projects_manager.project.models import Project
-from projects_manager.user.models import ProjectUser
 from projects_manager.user.serializers import UserNameSerializer
+
+
+def validate_no_special_characters(value):
+    pattern = r'^(?!.*[£$%&*]+.*[£$%&*]+.*[£$%&*]+)[^£$%&*]+$'
+    if not re.match(pattern, value):
+        raise ValidationError('The entered value is incorrect.')
+
+
+def validate_comment(comment):
+    if len(comment) < 10:
+        raise ValidationError('Description must be at least 10 characters long.')
+    if len(comment) > 500:
+        raise ValidationError('Description cannot exceed 500 characters.')
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -18,11 +34,31 @@ class CommentSerializer(serializers.ModelSerializer):
 
 
 class CommentModifySerializer(serializers.ModelSerializer):
-    project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all(), allow_null=False, allow_empty=False)
-    user = serializers.PrimaryKeyRelatedField(queryset=ProjectUser.objects.all(), allow_null=False, allow_empty=False)
-    comment = serializers.CharField()
-    timestamp = serializers.IntegerField()
+    comment = serializers.CharField(max_length=500, allow_null=False, allow_blank=False, validators=[
+        validate_comment,
+        validate_no_special_characters,
+    ])
 
     class Meta:
         model = Comment
-        fields = ['id', 'project', 'user', 'comment', 'timestamp']
+        fields = ['comment']
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all(), allow_null=False, allow_empty=False)
+    comment = serializers.CharField(max_length=500, allow_null=False, allow_blank=False, validators=[
+        validate_comment,
+        validate_no_special_characters,
+    ])
+
+    class Meta:
+        model = Comment
+        fields = ['project', 'comment']
+
+    def create(self, validated_data):
+        request = self.context['request']
+        user = request.user
+        validated_data['user'] = user
+        validated_data['timestamp'] = datetime.now().timestamp()
+        comment = Comment.objects.create(**validated_data)
+        return comment
--- projects_manager/comment/views.py
+++ projects_manager/comment/views.py
@@ -1,30 +1,44 @@
 from rest_framework import permissions, viewsets
 
 from projects_manager.comment.models import Comment
-from projects_manager.comment.serializers import CommentSerializer, CommentModifySerializer
+from projects_manager.comment.serializers import CommentSerializer, CommentModifySerializer, CommentCreateSerializer
+from projects_manager.project.models import Project
 
 
 # Create your views here.
 
 
-class SomePermission(permissions.BasePermission):  # ToDo: Better name
-    message = 'User can modify self projects only'
+class CommentAccessPermission(permissions.BasePermission):
+
+    def __init__(self):
+        self.message = 'User can add comments only to assigned projects'
 
     def has_permission(self, request, view):
+        if request.user not in Project(request.data['project']).users.all():
+            return False
         if request.user.is_authenticated:
             return True
         return False
 
     def has_object_permission(self, request, view, obj):
-        return True
+        if request.user.is_superuser:
+            return True
+        if request.method not in ['GET', 'POST']:
+            self.message = "You can only add and show comments"
+            return False
+        if request.user in obj.project.users.all():
+            return True
+        return False
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-timestamp')
-    permission_classes = [SomePermission]
+    permission_classes = [CommentAccessPermission]
 
     def get_serializer_class(self):
-        serializer = CommentModifySerializer
-        if self.request.method in ['GET', ]:
-            serializer = CommentSerializer
-        return serializer
+        request_serializer_map = {
+            "POST": CommentCreateSerializer,
+            "PUT": CommentModifySerializer,
+            "PATCH": CommentModifySerializer
+        }
+        return request_serializer_map.get(self.request.method, CommentSerializer)
