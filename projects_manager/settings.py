a006f5e59ea615045f2026bfb35c89db26c42e6b
MKan <kanabaj.maciej@gmail.com>
2023-06-08 02:36:57 +0200

    config

--- projects_manager/settings.py
+++ projects_manager/settings.py
@@ -146,10 +146,11 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://manager-drf-api.herokuapp.com"
+]
 
 CORS_ALLOW_HEADERS = [
     'accept',
