ad19748995013864a96baccce03388ee9ed99bec
MKan <kanabaj.maciej@gmail.com>
2023-06-08 02:41:32 +0200

    config

--- projects_manager/settings.py
+++ projects_manager/settings.py
@@ -142,14 +142,15 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://manager-drf-api.herokuapp.com"
+    "https://manager-drf-api.herokuapp.com",
+    "https://mkan-manager.herokuapp.com"
 ]
 
 CORS_ALLOW_HEADERS = [
